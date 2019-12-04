9969c73c01951bb2d64cbad8140db788bd0b5bc6
Dale Halling <hrangar@protonmail.com>
2019-12-04 15:13:35 -0600

    add cat personality test concern

--- app/controllers/cat_personality_test_controller.rb
+++ app/controllers/cat_personality_test_controller.rb
@@ -1,15 +1,18 @@
 class CatPersonalityTestController < ApplicationController
 
+  include Result
+
   def new
     puts "SESSSION"
     @answer = session[:answer]
   end
 
   def create
-    puts 'LLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL'
-    puts params
-    puts 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
-  
+    #somethingfoo(params)
+    catNumber = catPersonality(params)
+    puts "cat number"
+    puts catNumber
+
     redirect_to new_cat_personality_test_path
   end
 
